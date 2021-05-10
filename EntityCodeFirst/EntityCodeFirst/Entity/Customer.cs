522ce03d90fd3204d7b6a80a9ad83409ef187342
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-05-10 12:40:25 +0300

    Added Entity Code First Summaries

---
+++ EntityCodeFirst/EntityCodeFirst/Entity/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCodeFirst.Entity
+{
+    class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string CustomerTurkishId { get; set; }
+    }
+}
---
+++ EntityCodeFirst/EntityCodeFirst/Entity/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCodeFirst.Entity
+{
+    class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductBranch { get; set; }
+        public string ProductCategory { get; set; }
+        public int ProductStock { get; set; }
+        public Category Category { get; set; }
+    }
+}
